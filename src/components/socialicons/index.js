f32251bb08837486abfb1a9234a888a798562cb0
rjf5768 <rjf5768@psu.edu>
2024-09-03 17:41:22 -0400

    updated links

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -14,13 +14,7 @@ export const Socialicons = (params) => {
   return (
     <div className="stick_follow_icon">
       <ul>
-        {socialprofils.twitter && (
-          <li>
-            <a href={socialprofils.twitter}>
-              <FaTwitter />
-            </a>
-          </li>
-        )}
+        
         {socialprofils.github && (
           <li>
             <a href={socialprofils.github}>
@@ -28,13 +22,7 @@ export const Socialicons = (params) => {
             </a>
           </li>
         )}
-        {socialprofils.facebook && (
-          <li>
-            <a href={socialprofils.facebook}>
-              <FaFacebookF />
-            </a>
-          </li>
-        )}
+        
         {socialprofils.linkedin && (
           <li>
             <a href={socialprofils.linkedin}>
--- src/content_option.js
+++ src/content_option.js
@@ -1,23 +1,25 @@
+
+
 const logotext = "Rong Feng";
 const meta = {
     title: "Rong's Portfolio",
-    description: "I am a undergraduate student studying in Computer Science and minor in Supply Chain & Information Science of Technology",
+    description: "I am a undergraduate student at Penn State, studying in Computer Science and minor in Supply Chain & Information Science of Technology",
 };
 
 const introdata = {
-    title: "I’m Rong Feng",
+    title: "I'm Rong Feng",
     animated: {
         first: "I love coding",
         second: "I'm interested in algorithms and AI",
         third: "I code cool websites",
     },
-    description: "I am a undergraduate student studying in Computer Science and minor in Supply Chain & Information Science of Technology",
+    description: "I am a undergraduate student at Penn State, studying in Computer Science and minor in Supply Chain & Information Science of Technology",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "Self-Evaluation",
-    aboutme: " I am proactive, innovative, and diligent in my work, with a strong sense of responsibility. I am skilled atusing office automation software and excel at identifying, discovering, and solving problems at work, with strong analytical abilities. I am hands-on, conscientious, and have a strong sense of social responsibility. Although I am not very talkative, I am always able to pinpoint problems and propose solutions accurately. I have a solid foundation of theoretical knowledge and practical experience and strive to perform at my best and most stable state in every task. I enjoy taking on new challenges; every new product provides me with afresh experience and the opportunity to fully dedicate myself",
+    aboutme: " I am proactive, innovative, and diligent in my work, with a strong sense of responsibility. I am skilled atusing office automation software and excel at identifying, discovering, and solving problems at work, with strong analytical abilities. I am hands-on, conscientious, and have a strong sense of social responsibility. Although I am not very talkative, I am always able to pinpoint problems and propose solutions accurately. I have a solid foundation of theoretical knowledge and practical experience and strive to perform at my best and most stable state in every task. I enjoy taking on new challenges; every new product provides me with afresh experience and the opportunity to fully dedicate myself.",
 };
 const worktimeline = [{
         jobtitle: "Game Development (UE5)",
@@ -74,9 +76,9 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
+        img: "https://picsum.photos/400?grayscale",
         description: "My Resume",
-        link: "#",
+        link: "https://www.linkedin.com/in/rong-feng-205b022a1/details/featured/1725397836592/single-media-viewer/?profileId=ACoAAEj04qUBSeA_j2yPJfxxsI1vMf9Wn4vwk4U",
     },/*
     {
         img: "https://picsum.photos/400/800/?grayscale",
